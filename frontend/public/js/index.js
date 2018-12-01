b4fc8c40f1d14895d3e8f91088dd52eb3ac4e04d
felipe <marcelus20felipe@gmail.com>
2018-12-01 16:19:30 +0000

    - improvements on index.js
     - created LoginModels.js
    
     - Moved style.css to a new location
    
     - imported LoginModel class to index.html

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,41 +1,36 @@
+window.addEventListener("load", ()=>{
+    const select = (strId) => document.querySelector("#"+strId).value;
+    const loginForm = document.querySelector("#loginform");
 
-class LoginModel{
-    constructor(email, password){
-        this.email = email;
-        this.password = password;
-    }
-}
-const select = (strId) => document.querySelector("#"+strId).value;
-const loginForm = document.querySelector("#loginform");
+    loginForm.addEventListener("submit", (e)=>{
 
-loginForm.addEventListener("submit", (e)=>{
+        e.preventDefault();
 
-    e.preventDefault();
+        const email = select("email").trim();
+        const password = select("password").trim();
 
-    const email = select("email").trim();
-    const password = select("password").trim();
-
-    const loginForm = new LoginModel(email, password);
+        const loginForm = new LoginModel(email, password);
 
         fetch("/booking_platform/backend/controllers/login.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json; charset=utf-8"
-        },
-        body: JSON.stringify(loginForm)
-    }).then(response=>response.text())
-        .then(responseText=>{
-            if(parseInt(responseText ) === 1){
-                window.location.href = "http://localhost/booking_platform/dashboard.html";
-            }else{
-                const errorDiv = document.querySelector("#errorCred");
-                errorDiv.style.visibility = "visible";
-                setTimeout((()=> errorDiv.style.visibility = "hidden"), 2000);
-            }
-        }).catch(e=>{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify(loginForm)
+        }).then(response=>response.text())
+            .then(responseText=>{
+                if(parseInt(responseText ) === 1){
+                    window.location.href = "http://localhost/booking_platform/dashboard.html";
+                }else{
+                    const errorDiv = document.querySelector("#errorCred");
+                    errorDiv.style.visibility = "visible";
+                    setTimeout((()=> errorDiv.style.visibility = "hidden"), 2000);
+                }
+            }).catch(e=>{
             const errorDiv = document.querySelector("#errorCred");
             errorDiv.style.visibility = "visible";
             setTimeout((()=> errorDiv.style.visibility = "hidden"), 2000);
-    })
+        })
 
+    });
 });
---
+++ frontend/public/js/models/LoginModel.js
@@ -0,0 +1,6 @@
+class LoginModel{
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+    }
+}
