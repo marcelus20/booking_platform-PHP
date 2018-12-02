628a891c219c5f193d0aedf555d9f4a1d9d57327
felipe <marcelus20felipe@gmail.com>
2018-12-02 19:56:21 +0000

    - Created Customer.js class

---
+++ frontend/public/js/models/Customer.js
@@ -0,0 +1,9 @@
+class Customer {
+    constructor(c_id, first_name, last_name){
+        this.c_id = c_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+}
+
+const customer = ({c_id, first_name, last_name}) => new CustomerController();
