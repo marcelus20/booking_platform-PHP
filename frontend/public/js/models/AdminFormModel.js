1bd8b0ad35cf90ab33aca2641eba4dc419884801
felipe <marcelus20felipe@gmail.com>
2018-12-05 05:54:17 +0000

    created AdminFormModel for admin registration

---
+++ frontend/public/js/models/AdminFormModel.js
@@ -0,0 +1,9 @@
+
+class AdminFormModel {
+
+    constructor(email, password, confirmPassword) {
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+}
