62c3228ce50864dfef097883cf663e001e0cd8d7
felipe <marcelus20felipe@gmail.com>
2018-12-12 19:32:07 +0000

    added captcha to the system!

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -15,32 +15,38 @@ window.addEventListener("load", ()=>{
 
         e.preventDefault();
 
-        const email = select("email").value.trim();
-        const password = select("password").value.trim();
+        const captcha = select("recaptcha");
+        const response = grecaptcha.getResponse();
 
-        const loginForm = new LoginModel(email, password);
+        if(response.length == 0){
+            setAnElementClassToVisible("errorCaptcha");
+        }else{
+            const email = select("email").value.trim();
+            const password = select("password").value.trim();
 
-        fetch("/booking_platform/backend/routers/mainControllerRouter.php?executionType=login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            body: JSON.stringify(loginForm)
-        }).then(response=>response.text())
-            .then(responseText=>{
-                if(parseInt(responseText ) === 1){
-                    window.location.href = "http://localhost/booking_platform/dashboard.html";
-                }else{
-                    const errorDiv = select("errorCred");
-                    errorDiv.style.visibility = "visible";
-                    setTimeout((()=> errorDiv.style.visibility = "hidden"), 2000);
-                }
-            }).catch(e=>{
+            const loginForm = new LoginModel(email, password);
+
+            fetch("/booking_platform/backend/routers/mainControllerRouter.php?executionType=login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                },
+                body: JSON.stringify(loginForm)
+            }).then(response=>response.text())
+                .then(responseText=>{
+                    if(parseInt(responseText ) === 1){
+                        window.location.href = "http://localhost/booking_platform/dashboard.html";
+                    }else{
+                        const errorDiv = select("errorCred");
+                        errorDiv.style.visibility = "visible";
+                        setTimeout((()=> errorDiv.style.visibility = "hidden"), 2000);
+                    }
+                }).catch(e=>{
                 const errorDiv = select("errorCred");
                 errorDiv.style.visibility = "visible";
                 setTimeout((()=> errorDiv.style.visibility = "hidden"), 2000);
             })
-
+        }
 
     });
 
